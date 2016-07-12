9e0ab49056c7bdb0edab9bf7a706ac96abc6f62e
Dylan McDonald <dmcdonald21@cnm.edu>
2016-07-11 20:24:48 -0600

    prepared app before more :beers:

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,5 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppComponent } from './app/app.component';
-
-if (process.env.ENV === 'production') {
-	enableProdMode();
-}
+import { AppComponent } from './app.component';
 
 bootstrap(AppComponent, []);
